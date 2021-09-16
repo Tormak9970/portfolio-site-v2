db7cd1a2a079fb8f336e3a4c2b96ae0acc424204
Tormak <63308171+Tormak9970@users.noreply.github.com>
2021-09-15 21:02:04 -0500

    made progress on info section

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,47 @@
 <script lang="ts">
+	import MyInfo from "./Info.svelte";
+
 	export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="container" style="margin-right: 15px;">
+		<MyInfo/>
+	</div>
+	<div class="container" style="margin-left: 15px;">
+		<div class="color-tester" style="background-color: rgb(138, 194, 78);"></div>
+		<div class="color-tester" style="background-color: #82b74bff;"></div>
+		<div class="color-tester" style="background-color: #405d27ff;"></div>
+		<div class="color-tester" style="background-color: #32481eff;"></div>
+	</div>
 </main>
 
-<style>
+<style lang="scss">
+	$grey-primary: #252525;
+	$grey-secondary: #383838;
+	$bud-green: #82b74bff;
+    $bud-green__hover: rgb(138, 194, 78);
+    $dark-moss-green: #405d27ff;
+    $army-green: #32481eff;
+
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 100%;
+		height: 100%;
+
+		display: flex;
+		flex-direction: row;
+		align-items: center;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.container {
+		height: calc(100% - 60px);
+		width: calc(50% - 45px);
+
+		margin: 30px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.color-tester {
+		width: 50px;
+		height: 50px;
 	}
 </style>
---
+++ src/Info.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	
+</script>
+
+<div id="infoSection">
+	<h2 class="info-header">Information</h2>
+    <div class="info-container">
+        <div class="info-left">
+
+        </div>
+        <div class="info-right">
+
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+	$grey-primary: #252525;
+	$grey-secondary: #383838;
+	$bud-green: #82b74bff;
+    $bud-green__hover: rgb(138, 194, 78);
+    $dark-moss-green: #405d27ff;
+    $army-green: #32481eff;
+
+	#infoSection {
+		width: 100%;
+        max-width: 400px;
+		height: 30%;
+        min-height: 265px;
+
+		display: flex;
+		flex-direction: column;
+        align-items: center;
+
+        background-color: $grey-secondary;
+
+        border-radius: 8px;
+
+        box-shadow: #151515 1px 1px 10px 0px;
+	}
+
+    .info-header {
+        margin: 7px;
+
+        padding: 0;
+
+        font-size: 1.45em;
+
+        align-self: flex-start;
+    }
+    .info-container {
+        height: calc(100% - 49px);
+        width: calc(100% - 20px);
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+
+    }
+
+    .info-container > div {
+        width: calc(50% - 5px);
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
