b153334d4ecb636449beb13ae2ff5fe8b785ca5a
Tormak <63308171+Tormak9970@users.noreply.github.com>
2021-09-22 18:41:41 -0500

    working on organizations

--- src/components/Organizations.svelte
+++ src/components/Organizations.svelte
@@ -1,12 +1,80 @@
-<script lang="ts">
-	
+<script lang="ts" type="module">
+	import OrgsEntry from "./organizations/OrgsEntry.svelte";
+
+    interface OrgsPiece {
+        name:string,
+        path:string,
+        description:string,
+        hidden:boolean,
+        scrollType:string
+    }
+
+	interface ConfigProj {
+		name:String,
+		link:string
+	}
+
+    interface ConfigPiece {
+        name:string,
+        path:string,
+		about:String,
+        description:string,
+		projects:ConfigProj[]
+    }
+
+    let orgsPromise = loadEntries();
+
+    function loadEntries() {
+        return fetch('./config.json').then(response => { return response.json(); }).then(json => { return json['organizations']; });
+    }
+
+    const pieces:OrgsPiece[] = [];
+
+    let scrollIdx:number = 0;
+    let isScrolling:boolean = false;
+    function interceptScroll(e: WheelEvent) {
+        if (!isScrolling) {
+            const direction:boolean = e.deltaY > 0; // true = down, false = up
+            
+            const artElem = document.getElementById('art');
+            if (!(scrollIdx == 0 && !direction) && !(scrollIdx == artElem.children.length-1 && direction) && Math.abs(e.deltaY) != 0) {
+                isScrolling = true;
+                if (direction) {
+                    pieces[scrollIdx+1].scrollType = 'down-in';
+                    pieces[scrollIdx+1].hidden = false;
+                    pieces[scrollIdx].scrollType = 'down-out';
+                    setTimeout(() => {
+                        pieces[scrollIdx].hidden = true;
+                        scrollIdx++;
+                        isScrolling = false;
+                    }, 1500);
+                } else {
+                    pieces[scrollIdx-1].scrollType = 'up-in';
+                    pieces[scrollIdx-1].hidden = false;
+                    pieces[scrollIdx].scrollType = 'up-out';
+                    setTimeout(() => {
+                        pieces[scrollIdx].hidden = true;
+                        scrollIdx--;
+                        isScrolling = false;
+                    },  1500);
+                }
+            }
+        }
+    }
+
+    function processEntries(entr:ConfigPiece , i) { pieces.push({...entr, "hidden": true, scrollType: 'up-out'}); return entr; }
 </script>
 
-<div id="organizations">
-	<h2 class="organizations-header">Organizations</h2>
-    <div class="organizations-container">
-        
-    </div>
+<svelte:window on:wheel|stopPropagation="{interceptScroll}" />
+
+<div id="orgs">
+    {#await orgsPromise}
+        <div class="loading-data">Loading Organization pieces...</div>
+    {:then orgData} 
+        {#each orgData.map(processEntries) as artEntr, idx}
+            <OrgsEntry entryData={artEntr} hidden={pieces[idx].hidden} scrollType={pieces[idx].scrollType} isLast={idx+1 == orgData.length}/>
+        {/each }
+    {/await}
 </div>
 
 <style lang="scss">
@@ -14,13 +82,26 @@
 	$grey-secondary: #383838;
 	$bud-green: #82b74bff;
     $bud-green__hover: rgb(138, 194, 78);
+    $dark-moss-green: #405d27ff;
+    $army-green: #32481eff;
 
-	#organizations {
+    #orgs {
 		width: 80%;
 		height: 100%;
 
-		display: flex;
-		flex-direction: column;
-        align-items: center;
-	}
+        position: relative;
+
+        animation-name: fade-in;
+        animation-fill-mode: both;
+        animation-direction: alternate;
+        animation-duration: 1.5s;
+
+        overflow: hidden;
+    }
+
+
+    @keyframes fade-in {
+        0% { opacity: 0; }
+        100% { opacity: 1; }
+    }
 </style>
---
+++ src/components/organizations/OrgsEntry.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+    export let entryData:ConfigPiece;
+    export let hidden:boolean;
+    export let scrollType:string;
+    export let isLast:boolean;
+
+    interface ConfigPiece {
+        name:string,
+        path:string,
+        description:string
+    }
+</script>
+
+<div id="{entryData.name}" class="orgEntry{` ${scrollType}`}{hidden ? " hidden" : ""}">
+    <div class="org-header">
+        <h2>{entryData.name}</h2>
+    </div>
+    {#if entryData.path}
+        <div class="content-container">
+            <img src="{entryData.path}" alt="{entryData.name}">
+            <div class="description">
+                <p>{entryData.description}</p>
+            </div>
+        </div>
+    {:else}
+        <div class="description">
+            <p>{entryData.description}</p>
+        </div>
+    {/if}
+
+    <div class="msg">{isLast ? "" : "Scroll to continue..."}</div>
+</div>
+
+<style lang="scss">
+	$grey-primary: #252525;
+	$grey-secondary: #383838;
+	$bud-green: #82b74bff;
+    $bud-green__hover: rgb(138, 194, 78);
+
+    .orgEntry {
+        width: 80%;
+
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+
+        animation-fill-mode: both;
+        animation-direction: alternate;
+        animation-duration: 1.5s;
+
+        .org-header { font-size: 27px; }
+        .content-container {
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+            column-gap: 14px;
+            
+            img { width: 100%; height: auto; margin-top: 14px; border-radius: 5px; cursor: pointer; }
+            img:hover { opacity: 0.7; }
+        }
+        .description { width: 100%; margin-top: 14px; font-size: 24px; text-align: center; }
+        .msg { margin-top: 56px; font-size: 24px; }
+    }
+
+    .down-in { animation-name: down-fade-in; }
+    .up-in { animation-name: up-fade-in; }
+    .down-out { animation-name: down-fade-out; }
+    .up-out { animation-name: up-fade-out; }
+
+    .hidden { display: none; }
+
+    @keyframes down-fade-in { 0% { opacity: 0; transform: translate(-50%, 50%); } 100% { opacity: 1; transform: translate(-50%, -50%); } }
+    @keyframes down-fade-out { 0% { opacity: 1; transform: translate(-50%, -50%); } 100% { opacity: 0; transform: translate(-50%, -150%); } }
+
+    @keyframes up-fade-in { 0% { opacity: 0; transform: translate(-50%, -150%); } 100% { opacity: 1; transform: translate(-50%, -50%); } }
+    @keyframes up-fade-out { 0% { opacity: 1; transform: translate(-50%, -50%); } 100% { opacity: 0; transform: translate(-50%, 50%); } }
+</style>
