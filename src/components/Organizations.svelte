f76cf494d701c11826115ffe36d9d0105bf63391
Tormak9970 <63308171+Tormak9970@users.noreply.github.com>
2021-10-01 11:40:07 -0500

    jumpTo component functionality finished

--- src/components/Organizations.svelte
+++ src/components/Organizations.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" type="module">
 	import OrgsEntry from "./organizations/OrgsEntry.svelte";
+    import JumpList from "./utils/JumpList.svelte";
 
     type OrgsPiece = {
         name:string,
@@ -23,11 +24,7 @@
     }
 
     let orgsPromise = loadEntries();
-
-    function loadEntries(): Promise<ConfigPiece[]> {
-        return fetch('./config.json').then(response => { return response.json(); }).then(json => { return json['organizations']; });
-    }
-
+    function loadEntries(): Promise<ConfigPiece[]> { return fetch('./config.json').then(response => { return response.json(); }).then(json => { return json['organizations']; }); }
     const pieces:OrgsPiece[] = [];
 
     let scrollIdx:number = 0;
@@ -36,8 +33,8 @@
         if (!isScrolling) {
             const direction:boolean = e.deltaY > 0; // true = down, false = up
             
-            const artElem = document.getElementById('orgs');
-            if (!(scrollIdx == 0 && !direction) && !(scrollIdx == artElem.children.length-1 && direction) && Math.abs(e.deltaY) != 0) {
+            const orgsElem = document.getElementById('orgs');
+            if (!(scrollIdx == 0 && !direction) && !(scrollIdx == orgsElem.children.length-2 && direction) && Math.abs(e.deltaY) != 0) {
                 isScrolling = true;
                 if (direction) {
                     pieces[scrollIdx+1].scrollType = 'down-in';
@@ -61,8 +58,47 @@
             }
         }
     }
+    function interceptScrollFromIdx(direction:boolean) {
+        if (!isScrolling) {
+            isScrolling = true;
+            if (direction) {
+                pieces[scrollIdx+1].scrollType = 'down-in';
+                pieces[scrollIdx+1].hidden = false;
+                pieces[scrollIdx].scrollType = 'down-out';
+                setTimeout(() => {
+                    pieces[scrollIdx].hidden = true;
+                    scrollIdx++;
+                    isScrolling = false;
+                }, 1500);
+            } else {
+                pieces[scrollIdx-1].scrollType = 'up-in';
+                pieces[scrollIdx-1].hidden = false;
+                pieces[scrollIdx].scrollType = 'up-out';
+                setTimeout(() => {
+                    pieces[scrollIdx].hidden = true;
+                    scrollIdx--;
+                    isScrolling = false;
+                },  1500);
+            }
+        }
+    }
 
-    function processEntries(entr:ConfigPiece , i) { pieces.push({...entr, "hidden": i != 0, scrollType: i == 0 ? 'down-in' : 'up-out'}); return entr; }
+    function processEntries(entr:ConfigPiece , i) {
+        pieces.push({
+            ...entr,
+            "hidden": i != 0,
+            scrollType: i == 0 ? 'down-in' : 'up-out'
+        });
+        return entr;
+    }
+    function jumpToHandler(e: MouseEvent) {
+        const target:HTMLElement = <HTMLElement>e.currentTarget;
+        const curIdx = scrollIdx;
+        const tarIdx:number = parseInt(target.id.substr(0, target.id.indexOf('-')));
+        
+        scrollIdx = curIdx < tarIdx ? tarIdx-1 : tarIdx+1;
+        interceptScrollFromIdx(curIdx < tarIdx);
+    }
 </script>
 
 <svelte:window on:wheel|stopPropagation="{interceptScroll}" />
@@ -70,10 +106,11 @@
 <div id="orgs">
     {#await orgsPromise}
         <div class="loading-data">Loading Organization pieces...</div>
-    {:then orgData} 
-        {#each orgData.map(processEntries) as artEntr, idx}
-            <OrgsEntry entryData={artEntr} hidden={pieces[idx].hidden} scrollType={pieces[idx].scrollType} isLast={idx+1 == orgData.length}/>
+    {:then orgData}
+        {#each orgData.map(processEntries) as orgEntr, idx}
+            <OrgsEntry entryData={orgEntr} hidden={pieces[idx].hidden} scrollType={pieces[idx].scrollType} isLast={idx+1 == orgData.length}/>
         {/each}
+        <JumpList len={orgData.length} handler={jumpToHandler}/>
     {/await}
 </div>
 
---
+++ src/components/utils/JumpList.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	export let len: number;
+    export let handler:Function;
+
+    function wrapper(e: MouseEvent) {
+        handler(e);
+    }
+</script>
+
+<div id="jumpToCont">
+    {#each Array(len) as _, i}
+        <div>
+            <a id="{i}-jumpNav" on:click="{wrapper}">{i+1}</a>
+        </div>
+    {/each}
+</div>
+
+<!-- svelte-ignore unused-css-style-selector -->
+<style lang="scss">
+	$grey-primary: #252525;
+	$grey-secondary: #383838;
+	$bud-green: #82b74bff;
+    $bud-green__hover: rgb(138, 194, 78);
+
+    #jumpToCont {
+        z-index: 2;
+
+        position: absolute;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        background-color: $grey-secondary;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+
+        left: 0;
+        top: 8%;
+
+        
+        box-shadow: #151515 1px 1px 10px 0px;
+
+        div {
+            margin: 2px 4px;
+            cursor: pointer;
+
+            a {
+                width: 20px;
+                height: 20px;
+
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+
+                text-decoration: none;
+
+                font-size: 20px;
+            }
+        }
+
+        :first-child {
+            margin-top: 4px;
+        }
+
+        :last-child {
+            margin-bottom: 4px;
+        }
+    }
+</style>
