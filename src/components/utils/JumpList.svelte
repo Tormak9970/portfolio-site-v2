c33ba0129abc47068c8fac8c3e3eec41230d3788
Tormak <63308171+Tormak9970@users.noreply.github.com>
2021-10-09 12:48:44 -0500

    added tooltips to the jumplist

--- src/components/utils/JumpList.svelte
+++ src/components/utils/JumpList.svelte
@@ -30,12 +30,13 @@
 <div id="jumpToCont">
     {#each Array(len) as _, i}
         <div>
-            <a id="{i}-jumpNav" class="{i == scrollIdx ? setLast(i) : ''}" on:click="{wrapper}">
+            <a id="{i}-jumpNav" class="tooltip {i == scrollIdx ? setLast(i) : ''}" on:click="{wrapper}">
                 {#if i == scrollIdx}
                     <i class="fas fa-circle"></i>
                 {:else}
                     <i class="far fa-circle"></i>
                 {/if}
+                <span class="tooltip-text">{tooltips.get(i)}</span>
             </a>
         </div>
     {/each}
@@ -47,6 +48,7 @@
 	$grey-secondary: #383838;
 	$bud-green: #82b74bff;
     $bud-green__hover: rgb(138, 194, 78);
+    $tooltip-color: rgb(71, 71, 71);
 
     #jumpToCont {
         z-index: 2;
@@ -92,18 +94,49 @@
                     width: 16px;
                     height: 16px;
                 }
+
+                &:hover { color: $bud-green__hover; }
             }
-            a:hover {
-                color: $bud-green__hover;
+            .tooltip {
+                position: relative;
+
+                .tooltip-text {
+                    white-space: nowrap;
+                    visibility: hidden;
+                    background-color: $tooltip-color;
+                    color: #fff;
+                    text-align: center;
+                    padding: 2px 6px;
+                    border-radius: 6px;
+
+                    /* Position the tooltip text */
+                    position: absolute;
+                    z-index: 1;
+                    top: -2px;
+                    left: 105%;
+                    margin-left: 5px;
+
+                    /* Fade in tooltip */
+                    opacity: 0;
+                    transition: opacity 0.3s;
+                    
+                    &::after {
+                        content: " ";
+                        position: absolute;
+                        top: 50%;
+                        right: 100%; /* To the left of the tooltip */
+                        margin-top: -5px;
+                        border-width: 5px;
+                        border-style: solid;
+                        border-color: transparent $tooltip-color transparent transparent;
+                    }
+                }
+
+                &:hover { .tooltip-text { visibility: visible; opacity: 1; } }
             }
         }
 
-        :first-child {
-            margin-top: 2px;
-        }
-
-        :last-child {
-            margin-bottom: 2px;
-        }
+        :first-child { margin-top: 2px; }
+        :last-child { margin-bottom: 2px; }
     }
 </style>
