62a867523108ff04dbbedbda3a1a450aa6d98dd7
Tormak9970 <63308171+Tormak9970@users.noreply.github.com>
2021-10-21 10:13:47 -0500

    fixed tetris logic

--- src/components/tetris/Point.ts
+++ src/components/tetris/Point.ts
@@ -56,13 +56,13 @@ class Point{
     ctx.beginPath();
     ctx.moveTo(x + radius.tl, y);
     ctx.lineTo(x + width - radius.tr, y);
-    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    // ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
     ctx.lineTo(x + width, y + height - radius.br);
-    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    // ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
     ctx.lineTo(x + radius.bl, y + height);
-    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    // ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
     ctx.lineTo(x, y + radius.tl);
-    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+    // ctx.quadraticCurveTo(x, y, x + radius.tl, y);
     ctx.closePath();
     if (fill) { ctx.fill(); }
     if (stroke) { ctx.stroke(); }
