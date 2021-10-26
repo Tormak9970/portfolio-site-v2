ebeb6bd0562c258d06303776a73e09457f643295
Tormak <63308171+Tormak9970@users.noreply.github.com>
2021-10-25 19:42:09 -0500

    about mobile layout implimented

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,25 +1,49 @@
 <script lang="ts">
-	
+	import MediaQuery from "./_utils/MediaQuery.svelte";
 </script>
 
-<div id="aboutSection">
-	<div class="container">
-        <div class="img-container">
-            <!-- svelte-ignore a11y-img-redundant-alt -->
-            <img src="img/profile pic temp.png" alt="temporary profile picture">
+
+<MediaQuery query="(orientation:landscape)" let:matches>
+    {#if matches}
+        <div id="aboutSection" class="landscape">
+            <div class="container">
+                <div class="img-container">
+                    <!-- svelte-ignore a11y-img-redundant-alt -->
+                    <img src="img/profile pic temp.png" alt="temporary profile picture">
+                </div>
+            </div>
+            <div class="container">
+                <p class="bio">
+                    I am an aspiring software engineer and game developer. I enjoy learning new concepts through 
+                    self guided projects, and sharing my knowledge with others. Additionally, I am one of the SWTOR Slicers collaborators, 
+                    develop new datamining and extraction tools for Star Wars The Old Republic, and do community development for 
+                    TORCommunity.com. My interest in programming started in highschool, and I have continued to expand my knowledge 
+                    and skills since. Along side being a developer, I also like to make art in my free time. I enjoy mixing my two 
+                    passions in a fusion of technology and art.
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="container">
-        <p class="bio">
-            I am an aspiring software engineer and game developer. I enjoy learning new concepts through 
-            self guided projects, and sharing my knowledge with others. Additionally, I am one of the SWTOR Slicers collaborators, 
-            develop new datamining and extraction tools for Star Wars The Old Republic, and do community development for 
-            TORCommunity.com. My interest in programming started in highschool, and I have continued to expand my knowledge 
-            and skills since. Along side being a developer, I also like to make art in my free time. I enjoy mixing my two 
-            passions in a fusion of technology and art.
-        </p>
-    </div>
-</div>
+    {:else}
+        <div id="aboutSection" class="mobile">
+            <div class="container">
+                <div class="img-container">
+                    <!-- svelte-ignore a11y-img-redundant-alt -->
+                    <img src="img/profile pic temp.png" alt="temporary profile picture">
+                </div>
+            </div>
+            <div class="bio-cont">
+                <p class="bio">
+                    I am an aspiring software engineer and game developer. I enjoy learning new concepts through 
+                    self guided projects, and sharing my knowledge with others. Additionally, I am one of the SWTOR Slicers collaborators, 
+                    develop new datamining and extraction tools for Star Wars The Old Republic, and do community development for 
+                    TORCommunity.com. My interest in programming started in highschool, and I have continued to expand my knowledge 
+                    and skills since. Along side being a developer, I also like to make art in my free time. I enjoy mixing my two 
+                    passions in a fusion of technology and art.
+                </p>
+            </div>
+        </div>
+    {/if}
+</MediaQuery>
 
 <style lang="scss">
 	$grey-primary: #252525;
@@ -29,7 +53,7 @@
     $dark-moss-green: #405d27ff;
     $army-green: #32481eff;
 
-	#aboutSection {
+	.landscape {
 		width: 80%;
 		height: 80%;
 
@@ -59,6 +83,45 @@
             }
         }
 	}
+
+    .mobile {
+        width: 100%;
+		height: 80%;
+
+		display: flex;
+		flex-direction: column;
+        align-items: center;
+
+        animation-name: fade-in;
+        animation-fill-mode: both;
+        animation-duration: 1.5s;
+
+        .container {
+            width: 50%;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            img {
+                border-radius: 50%;
+                width: 100%;
+            }
+        }
+
+        .bio-cont {
+            width: 80%;
+            text-align: center;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+            .bio { font-size: 18px; }
+        }
+    }
 
     @keyframes fade-in {
         0% { opacity: 0; }
