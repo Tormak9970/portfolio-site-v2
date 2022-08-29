9baeef83904046da32ee80b4009ae22b1fbcb367
Tormak <63308171+Tormak9970@users.noreply.github.com>
2022-08-29 10:04:19 -0400

    track complete

--- src/pages/_utils/JumpList.svelte
+++ src/pages/_utils/JumpList.svelte
@@ -55,7 +55,13 @@
     </div>
     {#if $orientation == 0}
         <div class="jump-nav-container">
-            {#each Array(len) as _, i}
+            <svg xmlns="http://www.w3.org/2000/svg">
+                <rect class="track" x="9px" y="2%" width="6px" rx="3px" height="96%" />
+                {#each Array(len) as _, i}
+                    <circle  />
+                {/each}
+            </svg>
+            <!-- {#each Array(len) as _, i}
                 <div>
                     <a id="{i}-jumpNav" class="tooltip {i == scrollIdx ? setLast(i) : ''}" on:click="{wrapper}">
                         {#if i == scrollIdx}
@@ -66,7 +72,7 @@
                         <span class="tooltip-text">{tooltips.get(i)}</span>
                     </a>
                 </div>
-            {/each}
+            {/each} -->
         </div>
     {/if}
 </div>
@@ -83,6 +89,7 @@
         align-items: center;
         left: 0;
         top: 8%;
+        bottom: 8%;
     }
     #jumpToCont .layout-toggle {
         display: flex;
@@ -148,59 +155,15 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        background-color: var(--foreground);
-        border-top-right-radius: 8px;
-        border-bottom-right-radius: 8px;
+
         background-color: var(--foreground);
         border-top-right-radius: 8px;
         border-bottom-right-radius: 8px;
         box-shadow: #151515 1px 1px 10px 0px;
+
+        height: calc(100% - 54px);
     }
-    #jumpToCont .jump-nav-container div { margin: 1px 2px; cursor: pointer; }
-    #jumpToCont .jump-nav-container div .selected { color: var(--highlight); fill: var(--highlight); }
-    #jumpToCont .jump-nav-container div a {
-        width: 20px;
-        height: 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        text-decoration: none;
-        font-size: 16px;
-    }
-    #jumpToCont .jump-nav-container div a i { width: 16px; height: 16px; }
-    #jumpToCont .jump-nav-container div a:hover { color: var(--highlight-hover); }
-    #jumpToCont .jump-nav-container div .tooltip { position: relative; }
-    #jumpToCont .jump-nav-container div .tooltip .tooltip-text {
-        white-space: nowrap;
-        visibility: hidden;
-        background-color: var(--foreground-hover);
-        color: #fff;
-        text-align: center;
-        padding: 2px 6px;
-        border-radius: 6px;
-        /* Position the tooltip text */
-        position: absolute;
-        z-index: 1;
-        top: -2px;
-        left: 105%;
-        margin-left: 5px;
-        /* Fade in tooltip */
-        opacity: 0;
-        transition: opacity 0.3s;
-    }
-    #jumpToCont .jump-nav-container div .tooltip .tooltip-text::after {
-        content: " ";
-        position: absolute;
-        top: 50%;
-        right: 100%;
-        /* To the left of the tooltip */
-        margin-top: -5px;
-        border-width: 5px;
-        border-style: solid;
-        border-color: transparent var(--foreground-hover) transparent transparent;
-    }
-    #jumpToCont .jump-nav-container div .tooltip:hover .tooltip-text { visibility: visible; opacity: 1; }
-    #jumpToCont .jump-nav-container :first-child { margin-top: 2px; }
-    #jumpToCont .jump-nav-container :last-child { margin-bottom: 2px; }
+
+    #jumpToCont .jump-nav-container svg { height: 100%; width: 24px; }
+    #jumpToCont .jump-nav-container svg .track { fill: var(--foreground-hover); }
 </style>
