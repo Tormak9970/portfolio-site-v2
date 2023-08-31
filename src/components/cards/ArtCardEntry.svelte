2a13b28c4f05e69a67052559537af44b928b1a66
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-08-31 13:44:02 -0400

    chore: fixed additional formatting issues

--- src/components/cards/ArtCardEntry.svelte
+++ src/components/cards/ArtCardEntry.svelte
@@ -1,66 +1,75 @@
 <script lang="ts">
-    import { imageModalData, showing } from "../../Stores";
-    export let entryData:Art;
+  import { imageModalData, showing } from "../../Stores";
+  export let entryData:Art;
 
-    function showImageModal() { $imageModalData = { "id": "artPreviewModal", "data": { "img": entryData.image, "name": entryData.name } }; setTimeout(() => { $showing = true; }, 30); }
+  function showImageModal() {
+    $imageModalData = {
+      "id": "artPreviewModal",
+      "data": {
+        "image": entryData.image,
+        "name": entryData.name
+      }
+    };
+    setTimeout(() => { $showing = true; }, 30);
+  }
 </script>
 
 <div class="card-entr">
-    <div class="name-cont">
-        {entryData.name}
-    </div>
-    <div class="img-cont">
-        <img src="{entryData.image}" alt="" on:click|stopPropagation="{showImageModal}">
-    </div>
-    <div class="desc-cont">
-        {@html entryData.description}
-    </div>
+  <div class="name-cont">
+    {entryData.name}
+  </div>
+  <div class="img-cont">
+    <img src="{entryData.image}" alt="" on:click|stopPropagation="{showImageModal}">
+  </div>
+  <div class="desc-cont">
+    {@html entryData.description}
+  </div>
 </div>
 
 <style>
-    @import "/theme.css";
+  @import "/theme.css";
 
-    .card-entr {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
+  .card-entr {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
 
-        width: 90%;
-        max-width: 330px;
-        height: fit-content;
+    width: 90%;
+    max-width: 330px;
+    height: fit-content;
 
-        background-color: var(--foreground);
-        border-radius: 4px;
+    background-color: var(--foreground);
+    border-radius: 4px;
 
-        margin: 14px;
-    }
+    margin: 14px;
+  }
 
-    .name-cont {
-        width: 90%;
+  .name-cont {
+    width: 90%;
 
-        text-align: center;
+    text-align: center;
 
-        font-size: 20px;
+    font-size: 20px;
 
-        margin-bottom: 7px;
-    }
+    margin-bottom: 7px;
+  }
 
-    .img-cont {
-        width: 90%;
+  .img-cont {
+    width: 90%;
 
-        margin-bottom: 7px;
-    }
+    margin-bottom: 7px;
+  }
 
-    .img-cont > img { width: 100%; }
+  .img-cont > img { width: 100%; }
 
-    .desc-cont {
-        width: 90%;
+  .desc-cont {
+    width: 90%;
 
-        text-align: center;
+    text-align: center;
 
-        font-size: 16px;
+    font-size: 16px;
 
-        margin-bottom: 5px;
-    }
+    margin-bottom: 5px;
+  }
 </style>
